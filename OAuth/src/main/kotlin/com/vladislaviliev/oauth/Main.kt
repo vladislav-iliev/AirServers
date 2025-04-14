35f024f8bbf5e1102b1d652a78016618725168b9
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-04-14 22:25:06 +0300

    Extract PKCE logger from App's main function

--- OAuth/src/main/kotlin/com/vladislaviliev/oauth/Main.kt
+++ OAuth/src/main/kotlin/com/vladislaviliev/oauth/Main.kt
@@ -1,11 +1,9 @@
 package com.vladislaviliev.oauth
 
 import com.vladislaviliev.oauth.pkce.PkceGen
-import org.slf4j.LoggerFactory
 import org.springframework.boot.runApplication
 
 fun main() {
-    val logger = LoggerFactory.getLogger("Pkce")
-    logger.info(PkceGen().genPixy().toString())
+    PkceGen().logPixy()
     runApplication<App>()
 }
--- OAuth/src/main/kotlin/com/vladislaviliev/oauth/pkce/PkceGen.kt
+++ OAuth/src/main/kotlin/com/vladislaviliev/oauth/pkce/PkceGen.kt
@@ -1,8 +1,9 @@
 package com.vladislaviliev.oauth.pkce
 
+import org.slf4j.LoggerFactory
 import java.security.MessageDigest
 import java.security.SecureRandom
-import java.util.Base64
+import java.util.*
 
 class PkceGen {
 
@@ -10,10 +11,12 @@ class PkceGen {
 
     private fun genChallenge(verifier: ByteArray) = MessageDigest.getInstance("SHA-256").digest(verifier)
 
-    fun genPixy(): PkcePair {
+    private fun genPixy(): PkcePair {
         val verifier = genVerifier()
         val challenge = genChallenge(verifier)
         fun encode(bytes: ByteArray) = Base64.getUrlEncoder().withoutPadding().encodeToString(bytes)
         return PkcePair(encode(challenge), encode(verifier))
     }
+
+    fun logPixy() = LoggerFactory.getLogger(this::class.simpleName).info(genPixy().toString())
 }
