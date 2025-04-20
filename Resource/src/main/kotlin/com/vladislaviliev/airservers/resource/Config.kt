f19dd157e4d84a09e8ade0168bf1f73acd01edc9
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-04-20 20:19:32 +0300

    Resource - swap authorisation rules

--- Resource/src/main/kotlin/com/vladislaviliev/airservers/resource/Config.kt
+++ Resource/src/main/kotlin/com/vladislaviliev/airservers/resource/Config.kt
@@ -1,8 +1,20 @@
 package com.vladislaviliev.airservers.resource
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @PropertySource("classpath:secrets.properties")
-class Config
+class Config {
+
+    @Bean
+    fun httpFilters(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { it.requestMatchers(HttpMethod.GET).permitAll().anyRequest().authenticated() }
+        return http.build()
+    }
+
+}
