90681c7d06a9dfb420d33442e8bf590dc5cfaf22
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-04-22 04:34:12 +0300

    OAuth - PersistedUserAuthority - now ID not set by authority entity

--- OAuth/src/main/kotlin/com/vladislaviliev/oauth/credentials/userAuthority/PersistedUserAuthority.java
+++ OAuth/src/main/kotlin/com/vladislaviliev/oauth/credentials/userAuthority/PersistedUserAuthority.java
@@ -1,22 +1,30 @@
 package com.vladislaviliev.oauth.credentials.userAuthority;
 
-import com.vladislaviliev.oauth.credentials.PersistedAuthority;
-import com.vladislaviliev.oauth.credentials.PersistedUserAuthorityId_;
-import jakarta.persistence.*;
+import com.vladislaviliev.oauth.credentials.authority.PersistedAuthority;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "User_Authority")
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class PersistedUserAuthority {
 
+    @NonNull
+    @NotNull
     @EmbeddedId
     private PersistedUserAuthorityId id;
 
     @ManyToOne
-    @MapsId(PersistedUserAuthorityId_.AUTHORITY_ID)
-    @JoinColumn(name = PersistedUserAuthorityId_.AUTHORITY_ID)
+    @JoinColumn(name = PersistedUserAuthorityId_.AUTHORITY_ID, insertable = false, updatable = false)
     private PersistedAuthority authority;
 
     @CreationTimestamp
--- OAuth/src/main/kotlin/com/vladislaviliev/oauth/credentials/userAuthority/PersistedUserAuthorityId.java
+++ OAuth/src/main/kotlin/com/vladislaviliev/oauth/credentials/userAuthority/PersistedUserAuthorityId.java
@@ -2,24 +2,22 @@ package com.vladislaviliev.oauth.credentials.userAuthority;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Embeddable
 
-@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 public class PersistedUserAuthorityId implements Serializable {
 
+    @NonNull
     @NotNull
     private Long userId;
 
+    @NonNull
     @NotNull
     private Long authorityId;
 }
