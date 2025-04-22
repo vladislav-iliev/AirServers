d8fa222d925363ae560cc1f799b742831f14d6d0
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-04-22 04:34:22 +0300

    Add salt storage

---
+++ OAuth/src/main/kotlin/com/vladislaviliev/oauth/credentials/salt/Salt.java
@@ -0,0 +1,35 @@
+package com.vladislaviliev.oauth.credentials.salt;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+public class Salt {
+
+    public static final String USER_PASSWORD = "user_password";
+
+    @Id
+    @NonNull
+    private String id;
+
+    @NonNull
+    @NotNull
+    @Column(nullable = false)
+    private String valuation;
+
+    @NonNull
+    @NotNull
+    @Column(nullable = false)
+    private String info;
+
+}
---
+++ OAuth/src/main/kotlin/com/vladislaviliev/oauth/credentials/salt/SaltDao.kt
@@ -0,0 +1,7 @@
+package com.vladislaviliev.oauth.credentials.salt
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface SaltDao : CrudRepository<Salt, String>
