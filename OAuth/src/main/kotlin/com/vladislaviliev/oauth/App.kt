311659d16a971f5e8bc0dc1cd38f17d1f5d3101f
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-04-22 04:32:21 +0300

    OAuth - remove Pkce logging from App class

--- OAuth/src/main/kotlin/com/vladislaviliev/oauth/App.kt
+++ OAuth/src/main/kotlin/com/vladislaviliev/oauth/App.kt
@@ -1,15 +1,6 @@
 package com.vladislaviliev.oauth
 
-import com.vladislaviliev.oauth.pkce.PkceGen
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class App {
-
-    @Bean
-    fun logPkce() = ApplicationRunner {
-        PkceGen().logPixy()
-    }
-}
+class App
